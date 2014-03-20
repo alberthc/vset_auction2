c68a057ed5ddd96a7328c9a582a55438b29ab6d5
Albert Cho <albert.hyun.cho@gmail.com>
2014-03-19 22:07:51 -0700

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+
+    it "should have the content 'Vset Auction'''" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Vset Auction')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Home')		
+    end
+
+  end
+
+  describe "Help" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'         
+      expect(page).to have_title('Help')        
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About VSET Auction'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About VSET Auction')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'         
+      expect(page).to have_title('About')        
+    end
+  end
+end
