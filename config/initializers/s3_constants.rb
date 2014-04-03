9499f195791919ee1b4b7efba9cd3ebc5564663a
Albert Cho <albert.hyun.cho@gmail.com>
2014-04-02 22:00:44 -0700

    removed s3_constants.rb

--- config/initializers/s3_constants.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/s3_constants.rb
-
-if !Rails.env.production?
-  S3_BUCKET_NAME = "vsetauction"
-  AWS_ACCESS_KEY_ID = "AKIAJRQ4TBVSZMGIGRKA"
-  AWS_SECRET_ACCESS_KEY = "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"
-end
