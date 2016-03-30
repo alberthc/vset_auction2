e2d1eeae743801e51bb7e7a05ea34b49ce252cae
Albert Cho <albert.hyun.cho@gmail.com>
2016-03-29 19:46:00 -0700

    cleanup auctions#show

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -8,37 +8,33 @@ class AuctionsController < ApplicationController
 
   def show
     @auction = current_auction
-    @auction_items = @auction.auction_items.includes(:bids).where(auction_id: current_auction.id)
-    # @bids = Bid.all
+    @auction_items = @auction.auction_items.includes(:bids).where(auction_id: current_auction.id).order('bids.id ASC')
+
     @total_pledged = 0
     @total_unpledged = 0
-
     # TODO: fix this later - don't hardcode school totals
     @total_USC = 0
     @total_UCLA = 0
     @total_UCI = 0
     @total_OTHER = 0
     for auction_item in @auction_items
-      @max_bid = auction_item.max_bid
-      if !@max_bid.nil?
-        @total_pledged += auction_item.max_bid
-        @user = auction_item.bids.last.user
-        if !@user.nil?
-          @user_info = @user.user_info
-          if !@user_info.nil?
-            @school = @user_info.school
-            if !@school.nil?
-              if @school == UserInfo::USC
-                @total_USC += auction_item.max_bid
-              elsif @school == UserInfo::UCLA
-                @total_UCLA += auction_item.max_bid
-              elsif @school == UserInfo::UCI
-                @total_UCI += auction_item.max_bid
-              elsif @school == UserInfo::OTHER
-                @total_OTHER += auction_item.max_bid
-              end
-            end
-          end
+      max_bid = auction_item.max_bid
+      if !max_bid.nil?
+        @total_pledged += max_bid
+
+        user = auction_item.bids.last.user
+        school = user.user_info.school
+        puts case school
+        when UserInfo::USC
+          @total_USC += max_bid
+        when UserInfo::UCLA
+          @total_UCLA += max_bid
+        when UserInfo::UCI
+          @total_UCI += max_bid
+        when UserInfo::OTHER
+          @total_OTHER += max_bid
+        else
+          puts "Warning: Attempting to calculate invalid school " + school
         end
       else
         @total_unpledged += auction_item.min_bid
